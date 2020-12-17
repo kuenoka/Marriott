909daf71759f373e92e8731f179116cba21cf6b1
Kingsley Enoka <kuenoka@mix.wvu.edu>
2020-12-17 00:30:14 -0600

    status view controller

---
+++ Marriott/View/StatusViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StatusViewController.swift
+//  Marriott
+//
+//  Created by Kingsley Enoka on 12/17/20.
+//
+
+import UIKit
+
+class StatusViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+}
