8eca9e711aa090a85a9c3327bdeac8823701c3ac
Jerry Ren <jerryren.ios@gmail.com>
2020-12-22 23:45:59 -0500

    new storyboard injected

---
+++ Marriott/SignInModule/SignViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SignViewController.swift
+//  Marriott
+//
+//  Created by Jerry Ren on 12/22/20.
+//
+
+import UIKit
+
+class SignViewController: UIViewController {
+    override func viewDidLoad(){super.viewDidLoad()
+        print("sign called")}}
