31f5bd8774907dd34795a584f16e001af46485da
Jerry Ren <jerryren.ios@gmail.com>
2021-01-06 19:02:48 -0500

    Folders' structural adjustment (conflict-prone, beware)

---
+++ Marriott/Networks/UrlProtocolClassOverridden.swift
@@ -0,0 +1,12 @@
+//
+//  UrlProtocolClassOverridden.swift
+//  NSDrivenByXCTTests
+//
+//  Created by Jerry Ren on 1/6/21.
+//
+
+import Foundation
+
+class UrlSimulatedProtocolClass: URLProtocol {
+
+}
