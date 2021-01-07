8d88c24909bd75c88f4e27e4e3ab96c5db6e8fce
Jerry Ren <jerryren.ios@gmail.com>
2021-01-06 19:17:11 -0500

    UrlSimulatedProtocolClass' creation

--- Marriott/Networks/UrlProtocolClassOverridden.swift
+++ Marriott/Networks/UrlProtocolClassOverridden.swift
@@ -1,6 +1,6 @@
 //
 //  UrlProtocolClassOverridden.swift
-//  NSDrivenByXCTTests
+//  NSDrivenByXCT
 //
 //  Created by Jerry Ren on 1/6/21.
 //
@@ -8,5 +8,18 @@
 import Foundation
 
 class UrlSimulatedProtocolClass: URLProtocol {
+    static var stubResponseData: Data?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+         return true
+    }
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    override func stopLoading() { }
 
+    override func startLoading() {
+        self.client?.urlProtocol(self, didLoad: UrlSimulatedProtocolClass.stubResponseData ?? Data())
+        self.client?.urlProtocolDidFinishLoading(self)
+    }
 }
